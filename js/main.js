1ed52fccfcfb6905777a8a850e6d1b944cc26fae
GABRIEL DIAS SANTIAGO <00001088944243sp@aluno.educacao.sp.gov.br>
2022-11-10 15:36:56 -0300

    first

--- js/main.js
+++ js/main.js
@@ -63,3 +63,35 @@ carousel2();
   x[slideIndex2-1].style.display = "block";
   setTimeout(carousel2, 2000); 
 }
+
+
+var now = new Date();
+var theevent = new Date("Dec 04 2022 00:00:01");
+var seconds = (theevent - now) / 1000;
+var minutes = seconds / 60;
+var hours = minutes / 60;
+var input = document.getElementById('days')
+var days = hours / 24;
+ID = window.setTimeout("update();", 1000);
+input.style.display = 'block'
+
+if (now == "Dec 03 2022") {
+  document.contagemregressiva.days.value = 'Faltam poucas horas'
+  if (now <= "Dec 02 2022"  ){
+    document.contagemregressiva.days.value = document.contagemregressiva.days.value
+  }
+}
+
+function update() {
+  now = new Date();
+  seconds = (theevent - now) / 1000;
+  seconds = Math.round(seconds);
+  minutes = seconds / 60;
+  minutes = Math.round(minutes);
+  hours = minutes / 60;
+  hours = Math.round(hours);
+  days = hours / 24;
+  days = Math.round(days);
+  document.contagemregressiva.days.value = 'Em apenas ' + days + ' dias';
+  ID = window.setTimeout("update();", 1000);
+}
